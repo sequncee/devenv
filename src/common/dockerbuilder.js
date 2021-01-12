c5ea970f0431c2d27e74941e19461f8872cafdcb
legiorange <legiorange@163.com>
2021-01-12 09:43:11 +0800

    预留

--- src/common/dockerbuilder.js
+++ src/common/dockerbuilder.js
@@ -1,2 +1,14 @@
 // docker 在最后一位
 
+
+curl -sS https://get.docker.com/ | sh
+
+
+python 
+
+sudo apt install python3-dev python3-pip python3-setuptools -y
+
+openjdk http://openjdk.java.net/install/
+
+nodejs
+https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
